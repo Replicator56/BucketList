fe7074f05ea1a896244f9f7edad0864235dfc0f0
Quentin PRIGENT <qprigent@campus-eni.fr>
2025-06-25 10:20:02 +0200

    Fix after review

--- src/Service/CensuratorService.php
+++ src/Service/CensuratorService.php
@@ -8,6 +8,11 @@ class CensuratorService
         "bad", "wrong", "beautiful", "good", "pink", "chat"
     ];
 
+    /**
+     * Method purify allows to censure a sentence
+     * @param string $string
+     * @return array|string|string[]
+     */
     public function purify(string $string) {
 
         foreach(static::CENSURE_WORDS as $word) {
