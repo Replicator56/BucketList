a014b2f240cf3aca35b8429eb022a468d425132c
Quentin PRIGENT <qprigent@campus-eni.fr>
2025-06-25 10:18:18 +0200

    Fix after review

--- src/Service/CensuratorService.php
+++ src/Service/CensuratorService.php
@@ -4,21 +4,17 @@ namespace App\Service;
 
 class CensuratorService
 {
+    private const CENSURE_WORDS = [
+        "bad", "wrong", "beautiful", "good", "pink", "chat"
+    ];
+
     public function purify(string $string) {
 
-        $censuredWords = [
-            "bad", "wrong", "beautiful", "good", "pink", "chat"
-        ];
-
-        foreach($censuredWords as $word) {
+        foreach(static::CENSURE_WORDS as $word) {
             $replacement = str_repeat('*', mb_strlen($word));
             $string = str_ireplace($word, $replacement, $string);
         }
 
         return $string;
-
-
-
-
     }
 }
